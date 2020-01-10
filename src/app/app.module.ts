dbe573ac6d04673576ab673bd886e0ab28c52b4d
Richard Pitts <richard.pitts@umbragestudios.com>
2020-01-10 08:46:46 -0600

    feature: added firebase and successfully connected

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from  '@angular/fire';
+import { AngularFireAuthModule  } from '@angular/fire/auth';
+import { AngularFireDatabaseModule  } from '@angular/fire/database';
+import { environment } from 'src/environments/environment'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
